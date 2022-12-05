453ad99fd27d1b5cd23a86c1b3358a3d5af1f913
Azure Zhou <98193022+asyzhou@users.noreply.github.com>
2022-12-04 22:28:26 -0800

    heat map + predictions

--- test.py
+++ test.py
@@ -2,3 +2,14 @@ import numpy as np
 from sklearn import datasets
 # def calculateLoss():
 
+import seaborn as sns
+
+sns.set_theme()
+
+tips = sns.load_dataset("tips")
+
+sns.relplot(
+    data=tips,
+    x="total_bill", y="tip", col="time",
+    hue="smoker", style="smoker", size="size",
+)
--- test_multilabel_nn.py
+++ test_multilabel_nn.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import torch
 import os
+import seaborn as sn
+from sklearn.metrics import confusion_matrix
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
@@ -41,12 +43,15 @@ def prediction_accuracy(truth, predicted):
 
 
 # get single prediction
-def get_prediction(x, subgenres):
-    predict = torch.round(model(x)).numpy()
+def get_prediction(x, subgenres, model):
+    x_features = torch.tensor(x,dtype=torch.float32)
+    res = torch.round(model(x_features))
+    res = torch.Tensor.cpu(res).detach().numpy()
+    idx = np.argpartition(res, -3)[-3:]
+
     labels = []
-    for i in range(len(predict)):
-        if predict[i] == 1:
-            labels.append(subgenres[i])
+    for i in idx:
+        labels.append(subgenres[i])
     print(labels)
     return labels
     
@@ -99,14 +104,19 @@ if __name__ == '__main__':
     X_test = full_test.iloc[:, : len(full_test.columns) - num_genres]
     Y_test = full_test.iloc[:, len(full_test.columns) - num_genres:]
     test_dataset = make_dataset(X_test.values, Y_test.values)
-    test_dataloader = DataLoader(dataset=test_dataset, shuffle=True, batch_size=32)
+    test_dataloader = DataLoader(dataset=test_dataset, shuffle=False, batch_size=32)
 
     model.eval()
     test_run_acc = []
     test_run_cost = []
+    y_predicts = []
+    y_truths = []
     for i, (x_test, y_test) in enumerate(test_dataloader):
         # get predictions
         y_pred = model(x_test)
+        y_predicts.extend((torch.max(torch.exp(y_pred), 1)[1]).data.cpu().numpy())
+        y_truths.extend(y_test.data.cpu().numpy())
+
         accuracy = []
         for k, d in enumerate(y_pred, 0): 
             acc = prediction_accuracy(torch.Tensor.cpu(y_test[k]), torch.Tensor.cpu(d))
@@ -117,5 +127,40 @@ if __name__ == '__main__':
     print('test set')
     print(cost)
     print(np.asarray(test_run_acc).mean())
+
+    # check on one specific case
+    listOfGenres = sorted(['rock---alternative', 'rock---indierock', 'rock---singersongwriter',
+                    'rock---classicrock', 'rock---poprock', 'rock---progressiverock', 'rock---rockabilly', 'rock---rocknroll',
+                    'rock---hardcorepunk', 'rock---punk', 'rock---newwave', 'rock---postpunk', 'rock---alternativerock',
+                    'rock---indie', 'rock---hardrock', 'rock---hairmetal', 'rock---artrock', 'rock---bluesrock','rock---alternativepunk',
+                    'rock---latinrock', 'rock---powerpop', 'rock---indiepop', 'rock---psychobilly',
+                    'rock---stonerrock', 'rock---glamrock', 'rock---aor', 'rock---psychedelicrock', 'rock---britpop', 'rock---newromantic',
+                    'rock---emo', 'rock---softrock', 'rock---grunge', 'rock---pianorock', 'rock---american', 'rock---rockabillysoul',
+                    'rock---krautrock', 'rock---noisepop', 'rock---stoner', 'rock---garagerock', 'rock---lofi', 'rock---spacerock',
+                    'rock---indiefolk', 'rock---alternativemetal', 'rock---guitarvirtuoso', 'rock---powerballad', 'rock---symphonicrock',
+                    'rock---rockballad', 'rock---arenarock', 'rock---protopunk', 'rock---numetal', 'rock---rapcore', 'rock---funkrock',
+                    'rock---folkpunk', 'rock---surfrock',
+                    'rock---anarchopunk', 'rock---stonermetal', 'rock---southernrock', 'rock---poppunk', 'rock---jamband',
+                    'rock---funkmetal', 'rock---madchester', 'rock---britishinvasion', 'rock---chamberpop', 'rock---russianrock',
+                    'rock---experimentalrock', 'rock---melodicrock', 'rock---postgrunge', 'rock---horrorpunk', 'rock---streetpunk',
+                    'rock---jazzrock', 'rock---symphonicprog', 'rock---glam', 'rock---acousticrock',
+                    'rock---psychedelicpop'])
+
+    cf_matrix = confusion_matrix(y_truths, y_predicts)
+    df_cm = pd.DataFrame(cf_matrix/np.sum(cf_matrix) *74, index = [i for i in listOfGenres],
+                     columns = [i for i in listOfGenres])
+    plt.figure(figsize = (12,7))
+    sn.heatmap(df_cm, annot=True)
+    plt.savefig('output.png')
+    
+    X_test = full_test.iloc[1, : len(full_test.columns) - num_genres]
+    Y_test = full_test.iloc[1, len(full_test.columns) - num_genres:]
+    a = get_prediction(X_test, listOfGenres, model)
+    
+    idx = np.argpartition(Y_test, -3)[-3:]
+    labels = []
+    for i in idx:
+        labels.append(listOfGenres[i])
+    print(labels)
 
 
